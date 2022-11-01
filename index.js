5ee156ec6426c2393d0bbfc351ef7c91194f707c
Tanishq50 <tanishqchawande50@gmail.com>
2022-11-01 14:55:51 +0530

    make it better

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ dotenv.config();
 app.use(express.json());
 app.use(BodyParser.json());
 app.use(cookieParser());
-app.use(cors({exposedHeaders: ["set-cookie"]}));
+app.use(cors());
 
 //----------------------------* API's *----------------------------//
 
